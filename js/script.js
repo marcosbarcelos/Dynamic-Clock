40bfca5178929915945e6c353662e7f98ded9d91
marcosbarcelos <marcosbarcelosdev@gmail.com>
2022-04-02 20:10:55 -0300

    :ok_hand: style: code review

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const inSeconds = document.getElementById('inSeconds');
 
 const clock = setInterval(function time() {
   let dateToday = new Date();
-  let hours = String(dateToday.getHours()).padStart('2', '0');;
-  let minutes = String(dateToday.getMinutes()).padStart('2', '0');;
+  let hours = String(dateToday.getHours()).padStart('2', '0');
+  let minutes = String(dateToday.getMinutes()).padStart('2', '0');
   let seconds = String(dateToday.getSeconds()).padStart('2', '0');
 
   inHours.textContent = hours;
   inMinutes.textContent = minutes;
-  inSeconds.textContent = seconds
-})
+  inSeconds.textContent = seconds;
+});
